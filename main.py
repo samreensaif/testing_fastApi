524033da8f51d1d9367214b350720f236a18a9b4
Samreen Saif <samreensaif81@gmail.com>
2025-04-06 17:33:04 +0500

    CRUD operation usinf FastApi

---
+++ main.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+names = {"Samreen":22, "Muhammad":87, "Rahul":19, "Naveed":23}
+
+# get request
+
+@app.get("/")
+
+def get_request():
+    
+    new_names = [i.capitalize()+" having age " + str(j) for i,j in names.items()]
+    return new_names
+
+
+# post request
+
+
+class Item(BaseModel):
+    name: str
+    age: int
+    
+@app.post("/")
+
+def post_request(data:Item):
+
+    names.update({data.name:data.age})
+    # names[data.name]=data.age
+    
+    return names #names
+
+# delete request
+
+@app.delete("/{name}")
+
+
+def delete_request(name: str):
+    if name in names:
+        age = names.pop(name)
+        return {
+            "message": f"{name} having age {age} is deleted successfully!!! 👌"
+        }
+    else:
+        return {
+            "error": f"{name} not found in records!"
+        }
+
+
+# put request
+
+@app.put("/{name}")
+def put_request(name: str, data: Item):
+    names[name] = data.age
+    return {
+        "message": f"{name} updated to age {data.age} successfully! ✅",
+        "names": names
+    }
